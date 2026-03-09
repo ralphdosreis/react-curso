54f2e41d26f2866ab71d0718d2d770b03d208438
Ralph dos Reis Torres <ralphdosreistorres@gmail.com>
2026-03-09 14:45:08 -0300

    add propriedades-ex

---
+++ propriedades-ex/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Home';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <section>
+      <Header />
+      <Home />
+    </section>
+  );
+};
+
+export default App;
---
+++ propriedades-ex/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Titulo from './Titulo';
+
+const Home = () => {
+  return (
+    <section>
+      <Home texto="Home" />;<p>Essa é a Home do site</p>
+    </section>
+  );
+};
+
+export default Home;
