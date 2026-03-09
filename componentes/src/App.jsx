feaa2541910ace738b2978dd77f80178aa799331
Ralph dos Reis Torres <ralphdosreistorres@gmail.com>
2026-03-09 10:08:11 -0300

    componentes form

--- componentes/src/App.jsx
+++ componentes/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Form from './Form/Form';
 
 const App = () => {
   return (
-    <div>
+    <React.Fragment>
       <Header />
       <Footer />
-    </div>
+      <Form />
+    </React.Fragment>
   );
 };
 
